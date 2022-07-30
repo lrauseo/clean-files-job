69ae18583d560d37dd994afd96b300a3bacdc8ba
Leandro Rauseo <leandro.rauseo@gmail.com>
2022-07-30 14:10:23 -0300

    projeto inicial

---
+++ src/main/java/com/lrrauseo/cleanfiles/CleanFilesJobApplication.java
@@ -0,0 +1,13 @@
+package com.lrrauseo.cleanfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanFilesJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanFilesJobApplication.class, args);
+	}
+
+}
